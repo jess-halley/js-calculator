77d4d8af7cbcb0942d7b5f09fde401ed401d4813
Jess Halley <jmmhalley@gmail.com>
2017-04-11 10:14:26 +1200

    add pseudocode

--- calculator.js
+++ calculator.js
@@ -1,7 +1,12 @@
+
+// I needed three global variables for this program, total holds results of any calculations,
+//currentNum, holds any digits entered piror to calculations
+// and op, is a sting which holds my numbers
 var total = 0;
 var currentNum = 0;
 var op = "";
 
+// This function clears the calculator, setting all of my global variables back to 0
 function clearCalculator(){
   total = 0;
   currentNum = 0;
@@ -9,6 +14,9 @@ function clearCalculator(){
   document.getElementById("display").innerHTML = 0;
 }
 
+//This function takes two digits and joins them together so that the program reads them as a whole number. Eg 12 instead of 1 & 2.
+// To do that I turned both digits into strings and pushed them together.
+//I then turned that string back into a number using parseInt.
 function concatenateDigits(input){
   var concat = String(currentNum) + String(input);
   currentNum = parseInt(concat);
@@ -16,6 +24,10 @@ function concatenateDigits(input){
   return;
 }
 
+//This function does my calculations. I use the variable operator for the first operator entered then in the switch I call the global variable op.
+//I'm using these two variables so that the program will wait for the subsquent digits instead of instantly doing the math once the opperator is entered.
+//This means its possible to enter mutliple operators.
+// op is then given the value of opperator and currentNum goes back to 0
 function calculate(operator){
   switch (op){
     case '+':
